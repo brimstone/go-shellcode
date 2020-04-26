775f1f0db98f8c74239ae2701950cca262f4bf5e
Matt Robinson <brimstone@the.narro.ws>
2020-04-26 12:07:10 -0400

    Support loading from a file and not just hex

--- cmd/sc/main.go
+++ cmd/sc/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	shellcode "github.com/brimstone/go-shellcode"
@@ -16,13 +17,19 @@ import (
 func main() {
 
 	if len(os.Args) != 2 {
-		fmt.Printf("Must have shellcode\n")
+		fmt.Printf("Must have shellcode of file\n")
 		os.Exit(1)
 	}
-	sc, err := hex.DecodeString(os.Args[1])
-	if err != nil {
-		fmt.Printf("Error decoding arg 1: %s\n", err)
-		os.Exit(1)
+
+	// First, try to read the arg as a file
+	sc, err := ioutil.ReadFile(os.Args[1])
+	if os.IsNotExist(err) {
+		// If that fails, try to interpret the arg as hex encoded
+		sc, err = hex.DecodeString(os.Args[1])
+		if err != nil {
+			fmt.Printf("Error decoding arg 1: %s\n", err)
+			os.Exit(1)
+		}
 	}
 
 	shellcode.Run(sc)
